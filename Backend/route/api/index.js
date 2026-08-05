61ba0aad08dfb07b590a3b717ae270b824d7497c
ashiqul290 <ashiquldeveloper11@gmail.com>
2026-08-06 01:23:47 +0600

    update

--- Backend/route/api/index.js
+++ Backend/route/api/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const auth = require('./auth');
 const team = require('./team');
+const service = require('./service');
 const api = express.Router();
 
 api.use('/auth', auth);
 api.use('/team', team);
-
+api.use('/service',service)
 
 module.exports = api;
---
+++ Backend/route/api/service/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const service = express.Router();
+
+// service.post('/add-service',)
+
+module.exports = service;
